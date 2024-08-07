b1826cda28b2cdace1b0e1118811f1e6b6c0831b
thedicrey <dicreyraptor@gmail.com>
2024-08-06 19:11:10 -0500

    0.1

---
+++ src/scope/function.js
@@ -0,0 +1,10 @@
+function greeting(){
+    let username = "Dicrey"
+    
+    if (username === "Dicrey"){
+        console.log(`hello ${username}!`)
+    }
+}
+
+greeting()
+// console.log(username)
---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName  //valor undefined
+firstName = "Cristian"
+console.log(firstName)
+
+var lastName = "David"
+lastName = "Ana"
+console.log(lastName)
+
+var secondName = "David"
+var secondName = "Ana"
+
+console.log(secondName)
+
+// let 
+let fruit = "Apple"
+fruit = "Kiwi"
+console.log(fruit)
+
+// let fruit = "Banano"
+console.log(fruit)
+
+// const
+const animal = "Dog"
+// const animal = "Cat"
+console.log(animal)
+
+const vehicles = []
+vehicles.push("Skyline","Supra")
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
